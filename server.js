6c419d537aa01ff48be6fd1c7c3d397dffed6be7
erick <erick_fp@p@hotmail.com>
2021-11-01 20:30:20 -0600

    changed authorization again

--- server.js
+++ server.js
@@ -15,13 +15,12 @@ const image = require('./ServerControllers/image');
 //database conection to front end
     //take in mind that CLARIFAI API takes too much updates times, 
     //it might fail to load the API since the API server goes down sometimes
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
     const db = knex({ 
     client: 'pg',
     connection: {
       connectionString: process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false
-      }
+      ssl: true
     }
 });
 
